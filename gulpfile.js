0e466caefcef891316fa723688ea0857d12d8e97
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-03-16 12:44:58 +0100

    jade ==> pug2 | gulpfile.js update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var   gulp  = require('gulp');
+var   gutil = require('gulp-util');
+var   sass  = require('gulp-sass');
+const pug   = require('gulp-pug2')
+
+gulp.task('pug', function() {
+    return gulp.src('./_pug/*.pug')
+        .pipe(pug({ yourTemplate: 'Locals' }))
+        .pipe(gulp.dest('./_includes'))
+});
 
 gulp.task('sass', function () {
-  return gulp.src('./DEV/sass/*.sass')
+  return gulp.src('./_sass/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assests'));
+    .pipe(gulp.dest('./assets'));
 });
 
 gulp.task('default', function () {
-  gulp.watch('./DEV/sass/*.sass', ['sass']);
+  gulp.watch('./_sass/*.sass', ['sass']);
+  gulp.watch('./_pug/*.pug', ['pug'])
 });
