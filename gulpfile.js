8c208721c4372bd37fb529ce5c684671c1ead9f5
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-03-16 10:34:34 +0100

    Fix gulpfile.js | Remove jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return gulp.src('./DEV/sass/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./Shared'));
+    .pipe(gulp.dest('./assests'));
 });
 
 gulp.task('default', function () {
