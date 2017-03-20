22f141f24bb8ead69f618c471bd106076f5f90fa
Kissor Jeyabalan <kissor@sjbsolutions.no>
2017-03-20 23:06:52 +0100

    Restructured for unit testing

--- gulpfile.js
+++ gulpfile.js
@@ -19,18 +19,18 @@ gulp.task('reload', function() {
 gulp.task('pug', function() {
     return gulp.src('./_pug/*.pug')
         .pipe(pug({ yourTemplate: 'Locals' }))
-        .pipe(gulp.dest('./_includes'));
+        .pipe(gulp.dest('src/pages'));
 });
 
 gulp.task('sass', function () {
   return gulp.src(['./_sass/*.sass', './_sass/*.scss'])
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./assets'))
+    .pipe(gulp.dest('src/assets/css'))
     .pipe(browserSync.reload({ stream:true }));
 });
 
 gulp.task('default', ['browser-sync'], function () {
   gulp.watch(['./_sass/*.sass', './_sass/*.scss'], ['sass']);
   gulp.watch('./_pug/*.pug', ['pug']);
-  gulp.watch(['./_includes/*.html', './Scripts/*.js', '*.php'], ['reload']);
+  gulp.watch(['src/pages/*.html', 'src/assets/js/*.js', '*.php'], ['reload']);
 });
