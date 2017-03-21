4b08c1d97129006fc5c5467ad0073451cc5f1340
Kissor Jeyabalan <kissor@sjbsolutions.no>
2017-03-21 13:38:03 +0100

    Added index

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kissy
- * Date: 21.03.2017
- * Time: 13.16
- */
+error_reporting(E_ALL);
+
+// Opprett en session på IP hvis det ikke allerede eksisterer
+if(!(isset($_SESSION)))
+{
+    session_start();
+    $_SESSION['SESSION_IP'] = $_SERVER['REMOTE_ADDR'];
+}
+elseif ($_SESSION['SESSION_IP'] != $_SERVER['REMOTE_ADDR'])
+{
+    session_destroy();
+    die("Session Invalid");
+}
+
+
+$WebsiteEnabled = false;
+if($WebsiteEnabled) {
+    require_once("includes/config.php");
+
+    if (!isset($_GET['page'])) {
+        Header("Location: /main/home/");
+        die();
+    }
+
+    if (isset($_GET['page'])) {
+        switch ($_GET['page']) {
+            case 'home':
+                $pageTitle = "Hjem - Main";
+                require_once("pages/main.php");
+                break;
+        }
+    }
+}
+else
+{
+    echo "This website is currently disabled.</br>";
+}
