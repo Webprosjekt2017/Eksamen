90c61a505d8fb34a4dc4c31d336b35dfb8906c98
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-03-23 22:32:09 +0100

    Readability edit

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,9 +1,7 @@
-var nav = {
-  tansitionDuration: 500
-};
-
+var nav = {};
 
 $(document).ready(function() {
+
   // Add eventlistner 'mouseEnter' to navLinks
   nav.links = $('.navLink');
   nav.links.each(function(l) {
@@ -11,19 +9,23 @@ $(document).ready(function() {
       hoverController($(this)[0]);
     });
   });
+
   // find navLinkWrapper and add event 'mouseleave'
   nav.linkWrapper = $('.navLinkWrapper');
   nav.linkWrapper.mouseleave(function() {
     hoverController(nav.sel);
   })
+
   // replace nav.sel(index) with corresponding element
   nav.sel = nav.links[nav.sel];
+
   // Add div.selector to active tab
   var selector = document.createElement( "div" );
   selector.className = 'selector';
   nav.sel.append( selector );
   nav.selector = selector;
-});
+
+}); // END document ready function
 
 
 function hoverController(l){
