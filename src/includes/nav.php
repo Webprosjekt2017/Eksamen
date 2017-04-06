27d4fd022db716bbb21c25c91d51b1c393b6f5f0
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-04-06 11:46:58 +0200

    nav ready for filters

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,4 +1,8 @@
 
 <nav><img class="logo" src="/assets/imgs/LogoTransparent.png"/>
-  <div class="navLinkWrapper"><a class="navLink" href="#">Home</a><a class="navLink" href="#">About</a><a class="navLink" href="#">Contact</a><a class="navLink" href="#">Login</a></div>
+  <div class="navLinkWrapper">
+    <div id="navDrawerKnob">
+      <div class="arrow"></div>
+    </div><a class="navLink" href="#">Home</a><a class="navLink" href="#">About</a><a class="navLink" href="#">Contact</a><a class="navLink" href="#">Login</a>
+  </div>
 </nav>
