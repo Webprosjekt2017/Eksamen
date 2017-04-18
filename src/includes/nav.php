27bddca9ca1e3c719d056d863c037c52d3242017
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-04-18 15:33:22 +0200

    added tabindex
    added title and desc on map

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -3,6 +3,6 @@
   <div class="navLinkWrapper">
     <div id="navDrawerKnob">
       <div class="arrow"></div>
-    </div><a class="navLink" href="#">Home</a><a class="navLink" href="#">About</a><a class="navLink" href="#">Contact</a><a class="navLink" href="#">Login</a>
+    </div><a class="navLink" href="#" tabindex="1">Home</a><a class="navLink" href="#" tabindex="2">About</a><a class="navLink" href="#" tabindex="3">Contact</a><a class="navLink" href="#" tabindex="4">Login</a>
   </div>
 </nav>
--- src/pages/home.php
+++ src/pages/home.php
@@ -1,19 +1,26 @@
 
 <div class="container">
   <div class="map">
+    <div class="title">
+      <Lorum>Ipsum</Lorum>
+    </div>
+    <div class="desc">
+      <Lorem>ipsum dolor sit amet, consectetur adipisicing elit</Lorem>
+    </div>
     <div class="cover" id="mapCover"></div>
-    <div class="campus" id="Vulkan">
+    <div class="campus" id="Vulkan" tabindex="5">
       <div class="cover"></div>
       <h1>Vulkan</h1>
     </div>
-    <div class="campus" id="Fjerdingen">
+    <div class="campus" id="Fjerdingen" tabindex="6">
       <div class="cover"></div>
       <h1>Fjerdingen</h1>
     </div>
-    <div class="campus" id="Brenneriveien">
+    <div class="campus" id="Brenneriveien" tabindex="7">
       <div class="cover"></div>
       <h1>Brenneriveien</h1>
     </div>
+    <div class="scrollArrow"></div>
   </div>
 </div>
 <script>nav.sel = 0;</script>
