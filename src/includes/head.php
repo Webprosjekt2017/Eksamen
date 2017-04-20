7932d4ed6e2bc78b60b591da3cd5202ce12f705b
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-04-20 11:57:41 +0200

    added home css
    added fira sans font

--- src/includes/head.php
+++ src/includes/head.php
@@ -5,9 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <title></title>
   <!-- STYLESHEET-->
+  <link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet"/>
   <link rel="stylesheet" href="assets/css/main.css"/>
   <link rel="stylesheet" href="assets/css/map.css"/>
   <link rel="stylesheet" href="assets/css/nav.css"/>
+  <link rel="stylesheet" href="assets/css/home.css"/>
   <!-- SCRIPTS-->
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
   <script src="assets/js/nav.js"></script>
