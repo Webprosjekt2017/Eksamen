6f7c785e563da139f4e004b08537ce3107695901
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-04-20 11:58:41 +0200

    added 'overview' section

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,11 +1,13 @@
 
 <div class="container">
   <div class="map">
-    <div class="title">
-      <Lorum>Ipsum</Lorum>
-    </div>
-    <div class="desc">
-      <Lorem>ipsum dolor sit amet, consectetur adipisicing elit</Lorem>
+    <div class="mapText">
+      <div class="title">
+        <Lorum>Ipsum</Lorum>
+      </div>
+      <div class="desc">
+        <Lorem>ipsum dolor sit amet, consectetur adipisicing elit</Lorem>
+      </div>
     </div>
     <div class="cover" id="mapCover"></div>
     <div class="campus" id="Vulkan" tabindex="5">
@@ -21,6 +23,17 @@
       <h1>Brenneriveien</h1>
     </div>
     <div class="scrollArrow"></div>
+  </div>
+  <div class="wrapper">
+    <div class="row overview">
+      <div class="c2">
+        <h1>Overview</h1>
+        <p class="right">
+          <Lorem>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit</Lorem>
+        </p>
+      </div>
+      <div class="c2"></div>
+    </div>
   </div>
 </div>
 <script>nav.sel = 0;</script>
