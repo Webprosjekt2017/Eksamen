99f61d64a995a9838a60dc67960d7c58732aa5a9
Otard95 <stian.myklebostad@getmail.no>
2017-04-27 09:22:29 +0200

    updated nav

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -3,6 +3,7 @@ var nav = {
   linkWrapper: null,
   drawerKnow: null,
   selector: null,
+  selectorWidth: null,
   showFilter: function() {
     nav.linkWrapper.addClass('closed');
     nav.drawerKnow.addClass('show')
@@ -17,16 +18,14 @@ $(document).ready(function() {
 
   // Add eventlistner 'mouseEnter' to navLinks
   nav.links = $('.navLink');
-  nav.links.each(function(l) {
-    $(this).mouseenter(function() {
-      hoverController($(this)[0]);
-    });
+  nav.links.mouseenter(function() {
+    hoverController(this);
   });
 
   // find navLinkWrapper and add event 'mouseleave'
   nav.linkWrapper = $('.navLinkWrapper');
   nav.linkWrapper.mouseleave(function() {
-    hoverController(nav.sel);
+    hoverController(nav.sel[0]);
   });
 
   // Get nav drawer knob
@@ -36,17 +35,21 @@ $(document).ready(function() {
   });
 
   // replace nav.sel(index) with corresponding element
-  nav.sel = nav.links[nav.sel];
+  nav.sel = $(nav.links[nav.sel]);
 
   // Add div.selector to active tab
   var selector = document.createElement( "div" );
   selector.className = 'selector';
   nav.sel.append( selector );
-  nav.selector = selector;
+  nav.selector = $(selector);
+  nav.selectorWidth = Math.round( 100 * (nav.selector.width() / nav.sel.width()) ) / 100;
 
 }); // END document ready function
 
 
 function hoverController(l){
-  $(nav.selector).css('left', (l.offsetLeft - nav.sel.offsetLeft) );
+  nav.selector.css({
+      'left': (l.offsetLeft - nav.sel[0].offsetLeft) + 'px',
+      'width': $(l).width() * nav.selectorWidth + 'px'
+  });
 }
