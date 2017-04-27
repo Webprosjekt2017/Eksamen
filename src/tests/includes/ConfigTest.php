40175824b49e4912a992fccce3be3a23783abcd5
Kissor Jeyabalan <kissor@sjbsolutions.no>
2017-04-27 11:51:04 +0200

    Added assert

--- src/tests/includes/ConfigTest.php
+++ src/tests/includes/ConfigTest.php
@@ -6,4 +6,9 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         $config = new Config();
         $this->assertTrue($config->fluffFunction(7, 3));
     }
+
+    public function testFluffFunctionMaxIsLessThanMin() {
+        $config = new Config();
+        $this->assertFalse($config->fluffFunction(3, 7));
+    }
 }
