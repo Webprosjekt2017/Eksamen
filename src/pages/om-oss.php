d2e3168cf45da1e0bbbd7ddf4f37c7985845b759
Mikkelha <aromik16@student.westerdals.no>
2017-04-27 12:38:22 +0200

    Fixed about us page

--- src/pages/om-oss.php
+++ src/pages/om-oss.php
@@ -1,9 +1,13 @@
-<div id="aboutusbox">
-   <h1>
+<div class="row" id="aboutusbox">
+  <div class="c2">
+      <h1>
             Om oss
         </h1>
         <p id="aboutustext">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui autem esse poteris, nisi te amor ipse ceperit? Sine ea igitur iucunde negat posse se vivere? An haec ab eo non dicuntur? Quae autem natura suae primae institutionis oblita est? Laboro autem non sine causa; Duo Reges:
         </p>
+      </div>
+    <div class="c2">
+        <img id="aboutusimg" src="placeholder">
+    </div>
 </div>
-<img id="aboutusimg" src="placeholder">
