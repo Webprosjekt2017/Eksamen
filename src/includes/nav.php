248728c90848a2ded083fcd82790f996819b0a5c
Otard95 <stian.myklebostad@getmail.no>
2017-04-27 13:01:15 +0200

    added filter(work in progress)

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -5,4 +5,13 @@
       <div class="arrow"></div>
     </div><a class="navLink" href="#" tabindex="1">Hjem</a><a class="navLink" href="#" tabindex="2">Kart</a><a class="navLink" href="#" tabindex="3">Om Oss</a><a class="navLink" href="#" tabindex="4">Kontakt oss</a>
   </div>
+  <div class="filterWrapper">
+    <select class="filter" id="filter-type">
+      <option value="0">Hva ser du etter?</option>
+      <option value="Bar">Bar</option>
+      <option value="Restaurant">Restaurant</option>
+      <option value="Matbutikk">Matbutikk</option>
+    </select>
+    <input id="filter-name" type="text" placeholder="Kjenner du navnet?"/>
+  </div>
 </nav>
