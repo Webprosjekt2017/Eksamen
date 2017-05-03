3284f707c9fe5c1c4a5c6b65c6dbb0d1f272e9d2
Hannealm <almhan16@student.westerdals.no>
2017-05-03 21:34:31 +0200

    see_all - 2. utkast
    Endret på småting:
    La inn drop-down på åpningstider,
    Småfiks i koden,
    La alt inn i en wrapper.

--- src/pages/see_all.php
+++ src/pages/see_all.php
@@ -1,30 +1,42 @@
-<div class="row">
-    <div class="c2">
-        <h1>Vis alt</h1>
-    </div>
-</div>
 
+<div id="see_allcontainer">
+<h1>Vis alt</h1>
 <div class="row">
-    <div class="c4">
-        <div class="infokort">
-            <img class="kortbilde" src="assets/imgs/kiwi.png"S>
-            <h4>Tittel</h4>
-            <p>liten, kort og søt beskrivelse</p>
-            <div class="row">
-                <div class="c2">
-                    <p>Type</p>
+    <div class="c4 infokort">
+        <img class="kortbilde" src="assets/imgs/kiwi.png" S>
+        <h4>Tittel</h4>
+        <p>liten, kort og søt beskrivelse</p>
+        <div class="row">
+            <div class="c2">
+                <p>Type</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="c2">
+                <div class="open">
+                    <div class="status">Open now</div>
+                    <div class="toggleBtn" onclick="showTimes(this)" data-open="false"></div>
+                    <div class="times">
+                        <div class="row">
+                            <div class="c2">Mon - Fri</div>
+                            <div class="c2">10 - 22</div>
+                        </div>
+                        <div class="row">
+                            <div class="c2">Sat</div>
+                            <div class="c2">12 - 01</div>
+                        </div>
+                        <div class="row">
+                            <div class="c2">Sun</div>
+                            <div class="c2">Closed</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="c2">
-                    <p>Åpningstid</p>
-                </div>
-                <div class="c2">
-                    <p>Avstand</p>
-                </div>
+            <div class="c2">
+                <p>Avstand</p>
             </div>
-            
-            
+
         </div>
     </div>
+</div>
 </div>
