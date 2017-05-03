a45b2d660da636183a07f395d7d77749a4d85c69
Hannealm <almhan16@student.westerdals.no>
2017-05-03 20:52:25 +0200

    see_all - første utkast

--- src/pages/see_all.php
+++ src/pages/see_all.php
@@ -1,3 +1,30 @@
-<div class="your_class_here">
-  Content Here
+<div class="row">
+    <div class="c2">
+        <h1>Vis alt</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="c4">
+        <div class="infokort">
+            <img class="kortbilde" src="assets/imgs/kiwi.png"S>
+            <h4>Tittel</h4>
+            <p>liten, kort og søt beskrivelse</p>
+            <div class="row">
+                <div class="c2">
+                    <p>Type</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="c2">
+                    <p>Åpningstid</p>
+                </div>
+                <div class="c2">
+                    <p>Avstand</p>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
 </div>
