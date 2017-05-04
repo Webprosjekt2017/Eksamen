5df7bd677ac2f23b5f2decb950da08d473e34a69
Hannealm <almhan16@student.westerdals.no>
2017-05-04 13:04:30 +0200

    vis alt  - utkast 4
    fiks av feil størrelse på bokser på rader som ikke er fulle
    + litt småendringer på design.

--- src/pages/see_all.php
+++ src/pages/see_all.php
@@ -1,51 +1,49 @@
 <div id="see_allcontainer">
     <h1>Vis alt</h1>
-
-    <h3>Campus</h3>
-    <!--Overskrift som kommer mellom hver campus-bit-->
+    <h3>Campus</h3><!--Overskrift som kommer mellom hver campus-bit-->
     <div class="row">
-        <div class="c4 infokort">
-            <!--Infokort start-->
-            <img class="kortbilde" src="assets/imgs/kiwi.png" S>
-            <h4>Tittel</h4>
-            <p>liten, kort og søt beskrivelse</p>
-            <div class="row">
-                <div class="c2">
-                    <p>Type</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="c2 v-align-content">
-                    <!--Open drop-downmeny-->
-                    <div class="open">
-                        <div class="status">Open now</div>
-                        <div class="toggleBtn" onclick="showTimes(this)" data-open="false"></div>
-                        <div class="times">
-                            <div class="row">
-                                <div class="c2">Mon - Fri</div>
-                                <div class="c2">10 - 22</div>
-                            </div>
-                            <div class="row">
-                                <div class="c2">Sat</div>
-                                <div class="c2">12 - 01</div>
-                            </div>
-                            <div class="row">
-                                <div class="c2">Sun</div>
-                                <div class="c2">Closed</div>
-                            </div>
-                        </div>
+        <div class="c4 column h-center-content"><!--Infokort start-->
+            <div class="infokort">
+                <img class="kortbilde" src="assets/imgs/kiwi.png" S>
+                <h4>Tittel</h4>
+                <p>liten, kort og søt beskrivelse</p>
+                <a href="https://kiwi.no/">Gå til hjemmesiden</a>
+                <div class="row">
+                    <div class="c2">
+                        <!--Denne gjentas for hver tagg-->
+                        <p>Type</p>
                     </div>
                 </div>
-                <div class="c2">
-                    <p>Avstand</p>
+                <div class="row">
+                    <div class="c2 v-align-content"><!--Open drop-downmeny-->
+                        <div class="open">
+                            <div class="status">Open now</div>
+                            <div class="toggleBtn" onclick="showTimes(this)" data-open="false"></div>
+                            <div class="times">
+                                <div class="row">
+                                    <div class="c2">Mon - Fri</div>
+                                    <div class="c2">10 - 22</div>
+                                </div>
+                                <div class="row">
+                                    <div class="c2">Sat</div>
+                                    <div class="c2">12 - 01</div>
+                                </div>
+                                <div class="row">
+                                    <div class="c2">Sun</div>
+                                    <div class="c2">Closed</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!--Slutt på dropdown-->
+                    <div class="c2">
+                        <p>Avstand</p>
+                    </div>
                 </div>
-
             </div>
-        </div>
-        <!--infokort slutt-->
-    </div>
-    <!--Row slutt-->
+        </div><!--infokort slutt-->
+    </div><!--Row slutt-->
 
     <hr/>
     <!--Kommer over hvert campus, untatt det første-->
-</div> <!--Slutt wrapper for alt innhold-->
+</div>
+<!--Slutt wrapper for alt innhold-->
