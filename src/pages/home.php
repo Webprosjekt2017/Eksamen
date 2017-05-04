3ac214c0a0db6e115c8016b041922caaa0b64d18
Otard95 <stian.myklebostad@getmail.no>
2017-05-04 12:33:23 +0200

    ?

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,59 +1,39 @@
+
 <div class="container">
-    <div class="map">
-        <div class="backBtn"><img src="assets/imgs/DropDownArrow.png"/>Tilbake</div>
-        <div class="mapText">
-            <div class="title">
-                <Lorum>Ipsum</Lorum>
-            </div>
-            <div class="desc">
-                <Lorem>ipsum dolor sit amet, consectetur adipisicing elit</Lorem>
-            </div>
-        </div>
-        <div class="cover" id="mapCover"></div>
-        <div class="campus" id="Vulkan" tabindex="5">
-            <div class="cover"></div>
-            <h1>Vulkan</h1>
-        </div>
-        <div class="campus" id="Fjerdingen" tabindex="6">
-            <div class="cover"></div>
-            <h1>Fjerdingen</h1>
-        </div>
-        <div class="campus" id="Brenneriveien" tabindex="7">
-            <div class="cover"></div>
-            <h1>Brenneriveien</h1>
-        </div>
-        <div class="scrollArrow"></div>
+  <div class="map">
+    <div class="backBtn"><img src="assets/imgs/DropDownArrow.png"/>Tilbake</div>
+    <div class="mapText">
+      <div class="title">
+        <Lorum>Ipsum</Lorum>
+      </div>
+      <div class="desc">
+        <Lorem>ipsum dolor sit amet, consectetur adipisicing elit</Lorem>
+      </div>
     </div>
-    <div class="wrapper">
-        <div class="row overview">
-            <div class="c2">
-                <h1>Overview</h1>
-                <p class="right">
-                    <Lorem>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit
-                    </Lorem>
-                </p>
-            </div>
-            <div class="c2">something goes here</div>
-        </div>
+    <div class="cover" id="mapCover"></div>
+    <div class="campus" id="Vulkan" tabindex="5">
+      <div class="cover"></div>
+      <h1>Vulkan</h1>
     </div>
+    <div class="campus" id="Fjerdingen" tabindex="6">
+      <div class="cover"></div>
+      <h1>Fjerdingen</h1>
+    </div>
+    <div class="campus" id="Brenneriveien" tabindex="7">
+      <div class="cover"></div>
+      <h1>Brenneriveien</h1>
+    </div>
+    <div class="scrollArrow"></div>
+  </div>
+  <div class="wrapper">
+    <div class="row overview">
+      <div class="c2">
+        <h1>Overview</h1>
+        <p class="right">
+          <Lorem>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit</Lorem>
+        </p>
+      </div>
+      <div class="c2">something goes here</div>
+    </div>
+  </div>
 </div>
-<script>
-    $(document).ready(function () {
-        $("#mapCover").click(function (e) {
-            var x = e.pageX;
-            var y = e.pageY;
-            var offsetX = $(this).offset().left;
-            var offsetY = $(this).offset().top;
-            x = x - offsetX;
-            y = y - offsetY;
-            var mapY = $(this).height();
-            var mapX = $(this).width();
-            x = (x / mapX) * 100;
-            y = (y / mapY) * 100;
-            console.log("x: " + x.toFixed(3) + ", y: " + y.toFixed(3));
-        });
-    });
-</script>
