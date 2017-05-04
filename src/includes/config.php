b56e9bb1b8976bb43493a82cad7003d4c948b528
Otard95 <stian.myklebostad@getmail.no>
2017-05-04 17:41:35 +0200

    god damn config

--- src/includes/config.php
+++ src/includes/config.php
@@ -4,10 +4,10 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) header("Location: /");
 
 class Config {
     const
-        DB_HOST = '178.62.193.44',
-        DB_USER = 'jeykis16_woact',
-        DB_PASSWORD = 'DoraTheDatabaseExplorer12',
-        DB_DATABASE = 'jeykis16_woact',
+        DB_HOST = 'localhost',
+        DB_USER = 'root',
+        DB_PASSWORD = '',
+        DB_DATABASE = 'derp',
         REQUIRE_DB = true,
         WEBSITE_ONLINE = true;
 
