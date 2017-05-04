c0f8e3049934066e041b4cfaef8c504c93169f41
Otard95 <stian.myklebostad@getmail.no>
2017-05-04 15:04:20 +0200

    filter done

--- _js/filter.js
+++ _js/filter.js
@@ -10,25 +10,32 @@ var filter = {
     val: ''
   },
   map: function() {
-    if(this.type.val == '0' && this.name.val == '') {
-      this.resetLocs();
-      return false;
-    }
+
+    this.resetLocs();
+    loc.hide();
+
+    if(this.type.val == '0' && this.name.val == '') return false;
 
     $('.location').addClass('dim').each(function() {
-      if (filter.matchTag(this, filter.type.val) || filter.type.val == "0") {
-        var title = $(this).find('.title').text();
-        if(str_sim.compareTwoStrings(title, filter.name.val) > .5) {
+      if (filter.matchTag(this, filter.type.val) || filter.type.val == '0') {
+        var title = $(this).find('.hover-title').text().toLowerCase();
+        if(str_sim.compareTwoStrings(title, filter.name.val) > .4
+           || filter.name.val == ''
+           || title.indexOf(filter.name.val) > -1) {
           $(this).removeClass('dim').addClass('highlight');
         }
       }
     });
   },
   matchTag: function(dom, tag) {
+    var r = false;
     $(dom).find('.tags').children().each(function(){
-      if($(this).text() == tag) return true;
+      if($(this).text() == tag) {
+        r = true
+        return false;
+      }
     });
-    return false;
+    return r;
   },
   resetLocs: function() {
     $('.location').removeClass('highlight dim');
@@ -44,7 +51,7 @@ $(document).ready(function() {
     filter.map();
   });
   filter.name.input = $('#filter-name').on('keyup', function() {
-    filter.name.val = this.value;
+    filter.name.val = this.value.toLowerCase();
     filter.map();
   });
 
