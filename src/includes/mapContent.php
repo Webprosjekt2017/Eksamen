c851f5d811d2c056241c5428d2f0a599dca606db
Kissor Jeyabalan <kissor@sjbsolutions.no>
2017-05-04 15:24:06 +0200

    Added class for arrow

--- src/includes/mapContent.php
+++ src/includes/mapContent.php
@@ -23,6 +23,7 @@ foreach ($locations as $location) {
 
     <div class="location" id="<?= strtolower(preg_replace('/\s*/', '', $location['address'])) ?>">
         <div class="hover-title"><div></div><?= $location['title'] ?></div>
+        <div class="arr"></div>
         <div class="locInfo">
 
             <?php if (isset($location['images']['0']['path'])) { ?>
