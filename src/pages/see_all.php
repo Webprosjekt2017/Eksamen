f0c601886f641b593d2d7224b9b9f567258ae0da
Hannealm <almhan16@student.westerdals.no>
2017-05-04 11:17:19 +0200

    vis alt - 3. utkast
    Forbedret design

--- src/pages/see_all.php
+++ src/pages/see_all.php
@@ -1,7 +1,11 @@
 <div id="see_allcontainer">
     <h1>Vis alt</h1>
+
+    <h3>Campus</h3>
+    <!--Overskrift som kommer mellom hver campus-bit-->
     <div class="row">
         <div class="c4 infokort">
+            <!--Infokort start-->
             <img class="kortbilde" src="assets/imgs/kiwi.png" S>
             <h4>Tittel</h4>
             <p>liten, kort og søt beskrivelse</p>
@@ -11,8 +15,8 @@
                 </div>
             </div>
             <div class="row">
-                
-                <div class="c2"> <!--åpningstider-->
+                <div class="c2 v-align-content">
+                    <!--Open drop-downmeny-->
                     <div class="open">
                         <div class="status">Open now</div>
                         <div class="toggleBtn" onclick="showTimes(this)" data-open="false"></div>
@@ -32,11 +36,16 @@
                         </div>
                     </div>
                 </div>
-                
                 <div class="c2">
                     <p>Avstand</p>
                 </div>
+
             </div>
         </div>
+        <!--infokort slutt-->
     </div>
-</div>
+    <!--Row slutt-->
+
+    <hr/>
+    <!--Kommer over hvert campus, untatt det første-->
+</div> <!--Slutt wrapper for alt innhold-->
