469003ae7ec05f7d56e09289c3acfe8092235b38
Otard95 <stian.myklebostad@getmail.no>
2017-05-08 23:31:22 +0200

    added new logo to nav

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,5 +1,5 @@
 
-<nav><img class="logo" src="assets/imgs/LogoTransparent.png"/>
+<nav><img class="logo" src="assets/imgs/Logo.png"/>
   <div class="navLinkWrapper">
     <div id="navDrawerKnob">
       <div class="arrow"></div>
