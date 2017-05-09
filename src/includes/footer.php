149ededd015b4f166a407f3915aab1cf94923bb7
Otard95 <stian.myklebostad@getmail.no>
2017-05-09 14:13:13 +0200

    footer done

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,13 +1,25 @@
 
 <footer>
   <div class="row">
-    <div class="c3 column v-center-content">
-      <h4>Informasjon</h4>
-      <ul>
-        <li><a href="about"></a></li>
+    <div class="c3 column">
+      <h4>Kontakt info</h4>
+      <ul> 
+        <li>Tlf: 123 45 678</li>
+        <li>Addr: Christian Kroghs Gate 32A, Oslo</li>
+        <li> <a class="fanzy" href="contact">Kontakt oss</a></li>
       </ul>
     </div>
-    <div class="c3 column v-center-content"></div>
-    <div class="c3 column v-center-content"></div>
+    <div class="c3 column">
+      <h4>Resursser</h4>
+      <ul> 
+        <li><a href="https://github.com/aceakash/string-similarity"> <img class="miniLogo" src="assets/imgs/Git-Icon-White.png"/>String-similarity (npm)</a></li>
+        <li> <a href="https://github.com/deepak1556/gulp-browserify"> <img class="miniLogo" src="assets/imgs/Git-Icon-White.png"/>Gulp-browserify (npm)</a></li>
+        <li class="fontSmall"><a href="https://git-scm.com/downloads/logos">GitHub Logo</a> av <a href="https://twitter.com/jasonlong">Jason Long</a> under <a href="https://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a></li>
+      </ul>
+    </div>
+    <div class="c3 column"><img class="logo" src="assets/imgs/Logo.png"/>
+      <h4 class="fontLarge">Matvrak</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
   </div>
 </footer>
