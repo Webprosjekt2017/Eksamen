e2511bbe3e5549e550e4324082d57e2438f2efc4
Mikkelha <aromik16@student.westerdals.no>
2017-05-09 13:06:02 +0200

    updated about page

--- src/pages/about.php
+++ src/pages/about.php
@@ -5,6 +5,10 @@
         </h1>
         <p id="aboutustext">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui autem esse poteris, nisi te amor ipse ceperit? Sine ea igitur iucunde negat posse se vivere? An haec ab eo non dicuntur? Quae autem natura suae primae institutionis oblita est? Laboro autem non sine causa; Duo Reges:
+            <br>
+            <br>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui autem esse poteris, nisi te amor ipse ceperit? Sine ea igitur iucunde negat posse se vivere? An haec ab eo non dicuntur? Quae autem natura suae primae institutionis oblita est? Laboro autem non sine causa; Duo Reges
         </p>
       </div>
     <div class="c2">
