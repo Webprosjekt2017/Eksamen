74a63446261be7974a938a68ad99696a8fb964f8
Otard95 <stian.myklebostad@getmail.no>
2017-05-09 12:31:04 +0200

    work on footer
    nav - link to 'see all' href edit

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,7 +1,13 @@
 
 <footer>
   <div class="row">
-    <div class="c2 column v-center-content"></div>
-    <div class="c2 column v-center-content"></div>
+    <div class="c3 column v-center-content">
+      <h4>Informasjon</h4>
+      <ul>
+        <li><a href="about"></a></li>
+      </ul>
+    </div>
+    <div class="c3 column v-center-content"></div>
+    <div class="c3 column v-center-content"></div>
   </div>
 </footer>
