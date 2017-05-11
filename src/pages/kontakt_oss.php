ccaed09f5b071a6afeb2f620dd5839322a5c2634
akt95 <ashwin1504@hotmail.com>
2017-05-11 13:45:10 +0200

    Lagt inn horisontal og veritcal linje
    Endret på noen størrelser i tillegg.

--- src/pages/kontakt_oss.php
+++ src/pages/kontakt_oss.php
@@ -11,6 +11,21 @@
 
 
 <div class="row vertical">
+<div class="c2" id="contact_info">
+    <form action="/example.php">
+        Navn <br> 
+        <input type="text" name="firstname"> <br><br>
+        E-post<br>
+        <input type="text" name="e-mail"> <br><br>    
+        Emne<br>
+        <input type="text" name="subject" > <br><br>
+        Melding<br>
+        <input type="text" name="message" id="message_box"> <br> <br>        
+        <input type="submit" value="Send">
+                
+    </form>
+</div>  
+ 
     <div class="c2">
         <div id="info_text_left">
         <h3>Adresse: </h3>
@@ -24,26 +39,11 @@
         <h3>99 99 99 99</h3>
         <h3>matvrak@westerdals.no</h3>
     </div>
-    </div>
+    </div>    
+</div>
     
-    <div class="c2" id="contact_info">
-        <form action="/example.php">
-        
-            Navn <br>
-            <input type="text" name="firstname"> <br><br>
-            E-post<br>
-            <input type="text" name="e-mail"> <br><br>    
-            Emne<br>
-            <input type="text" name="subject" > <br><br>
-            Melding<br>
-            <input type="text" name="message" id="message_box"> <br> <br>
-            
-            
-            <input type="submit" value="Send">
-            </div>
-        
-        </form>
+    <div class="veritcalLine" id="vLine"> </div>
+    <div class="horisontalLine" id="hLine"> </div>
     
-    </div>   
-    </div>
-    </body>
+
+</body>
