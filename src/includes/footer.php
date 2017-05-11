76b57048705e5e93ad2ed87087f8b934be6151cc
Otard95 <stian.myklebostad@getmail.no>
2017-05-11 11:47:04 +0200

    fixed missing '.' in footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="c3 column">
       <h4>Kontakt info</h4>
-      <ul> 
+      <ul>
         <li>Tlf: 123 45 678</li>
         <li>Addr: Christian Kroghs Gate 32A, Oslo</li>
         <li> <a class="fanzy" href="contact">Kontakt oss</a></li>
@@ -11,7 +11,7 @@
     </div>
     <div class="c3 column">
       <h4>Resursser</h4>
-      <ul> 
+      <ul>
         <li><a href="https://github.com/aceakash/string-similarity"> <img class="miniLogo" src="assets/imgs/Git-Icon-White.png"/>String-similarity (npm)</a></li>
         <li> <a href="https://github.com/deepak1556/gulp-browserify"> <img class="miniLogo" src="assets/imgs/Git-Icon-White.png"/>Gulp-browserify (npm)</a></li>
         <li class="fontSmall"><a href="https://git-scm.com/downloads/logos">GitHub Logo</a> av <a href="https://twitter.com/jasonlong">Jason Long</a> under <a href="https://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a></li>
@@ -19,7 +19,7 @@
     </div>
     <div class="c3 column"><img class="logo" src="assets/imgs/Logo.png"/>
       <h4 class="fontLarge">Matvrak</h4>
-      <p>Matvrak etablert i 2017 i samarbeid med Westerdals Oslo ACT. Vi gjør hverdagen litt enklere for sultene studenter og ansatte etter en lang dag på campus</p>
+      <p>Matvrak etablert i 2017 i samarbeid med Westerdals Oslo ACT. Vi gjør hverdagen litt enklere for sultene studenter og ansatte etter en lang dag på campus.</p>
     </div>
   </div>
 </footer>
